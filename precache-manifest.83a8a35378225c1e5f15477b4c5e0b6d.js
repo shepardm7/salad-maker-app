e80fb3eb70e4c97fd69d17b0f6c3b5182811a594
Sateek Roy <sateeksuper@gmail.com>
2020-01-24 17:04:52 -0500

    Updates

---
+++ precache-manifest.83a8a35378225c1e5f15477b4c5e0b6d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c01e30ace07f7cfac7e6dc3bb002dfb",
+    "url": "/salad-maker-app/index.html"
+  },
+  {
+    "revision": "a41cb9b8fd98f3fac8b9",
+    "url": "/salad-maker-app/static/css/main.03e198df.chunk.css"
+  },
+  {
+    "revision": "1635fd885b4e84782ecf",
+    "url": "/salad-maker-app/static/js/2.1c40b263.chunk.js"
+  },
+  {
+    "revision": "89e0e1c6eb77d8f2fc102aff20d39ba2",
+    "url": "/salad-maker-app/static/js/2.1c40b263.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a41cb9b8fd98f3fac8b9",
+    "url": "/salad-maker-app/static/js/main.b027636e.chunk.js"
+  },
+  {
+    "revision": "fa68c52f60c7d22eaf6c",
+    "url": "/salad-maker-app/static/js/runtime-main.277c2a75.js"
+  },
+  {
+    "revision": "d3d731948c525278101a0d004d173954",
+    "url": "/salad-maker-app/static/media/aurgula.d3d73194.jpg"
+  },
+  {
+    "revision": "ac0c114a28224db38f395b034c18406a",
+    "url": "/salad-maker-app/static/media/cheddar-cheese.ac0c114a.jpg"
+  },
+  {
+    "revision": "591eb8d3d9f12f3ba30a15b1d81c9380",
+    "url": "/salad-maker-app/static/media/cucumber.591eb8d3.jpg"
+  },
+  {
+    "revision": "d58943c7129b88ee9080697f6cae042d",
+    "url": "/salad-maker-app/static/media/dressing.d58943c7.jpg"
+  },
+  {
+    "revision": "b0ab98b850c56a697fa0f156bbad8d81",
+    "url": "/salad-maker-app/static/media/feta-cheese.b0ab98b8.jpg"
+  },
+  {
+    "revision": "b43e4db0b2420d05817269073326d75a",
+    "url": "/salad-maker-app/static/media/green-pepper.b43e4db0.jpg"
+  },
+  {
+    "revision": "a5f46aa409a23e30a07649c6ab940adb",
+    "url": "/salad-maker-app/static/media/honey-mustard.a5f46aa4.jpg"
+  },
+  {
+    "revision": "467688634fdbb71d64554ac41d7bb14f",
+    "url": "/salad-maker-app/static/media/house-dressing.46768863.jpg"
+  },
+  {
+    "revision": "82a62f7d3cf0a23242b1dda995b0a29c",
+    "url": "/salad-maker-app/static/media/lettuce.82a62f7d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/salad-maker-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2f642faaa4fb7af27a03d238ee0d8642",
+    "url": "/salad-maker-app/static/media/mixed-greens.2f642faa.jpg"
+  },
+  {
+    "revision": "50b2a66e3fdeadead27db5d4180274b5",
+    "url": "/salad-maker-app/static/media/onion.50b2a66e.jpg"
+  },
+  {
+    "revision": "c0e1bb769dd5460ef8e08a68bff5d873",
+    "url": "/salad-maker-app/static/media/pickle.c0e1bb76.jpg"
+  },
+  {
+    "revision": "42ddc174e7cfc37a9d0d2f98b87d400e",
+    "url": "/salad-maker-app/static/media/pineapple.42ddc174.jpg"
+  },
+  {
+    "revision": "d77f06a0eb4411b3f48a07a2e5218244",
+    "url": "/salad-maker-app/static/media/ranch.d77f06a0.png"
+  },
+  {
+    "revision": "831621985ab176d74ab85f20b282d48f",
+    "url": "/salad-maker-app/static/media/salad-bowl.83162198.png"
+  },
+  {
+    "revision": "00cd63d8c01ef5d64bd1e6d5280732b9",
+    "url": "/salad-maker-app/static/media/spinach.00cd63d8.jpg"
+  },
+  {
+    "revision": "7ff655c7437a0b1893d073943274936c",
+    "url": "/salad-maker-app/static/media/tomato.7ff655c7.jpg"
+  },
+  {
+    "revision": "6f76cd3a9dab3b57cd7a737ae55c7362",
+    "url": "/salad-maker-app/static/media/toppings.6f76cd3a.jpg"
+  }
+]);
